46875b94665cdc9679eee33cf65c633e51ada03a
Sharon Brizinov <SharonBrizinov@users.noreply.github.com>
2020-10-27 18:13:34 +0200

    improvments

--- codemeter_port_check.py
+++ codemeter_port_check.py
@@ -1,26 +1,56 @@
 import socket
 import sys
-
-
-if len(sys.argv) != 2:
-	print("Usage: {} IP_ADDR".format(sys.argv[0]))
+try:
+	from netaddr import IPNetwork
+except Exception as e:
+	print("[-] Please install netaddr package.")
+	print("\tpython -m pip install netaddr --user")
 	sys.exit(1)
 
-ip_address = sys.argv[1]
-port = 22350
-timeout = 5
 
-
-sock = socket.socket(2,1)
-sock.settimeout(timeout)
+TIMEOUT_SEC = 1
+PORT = 22350
 try:
-	sock.connect((ip_address, port))
-	sock.send("73616d63000000004100010000000000".decode("hex"))
-	recv_data = sock.recv(1024)
-
-	if recv_data.startswith(b"samc"):
-		print("[V] CodeMeter instace detected!")
-	else:
-		print("[X] No CodeMeter instace detected")
+	PAYLOAD = bytes.fromhex("73616d63000000004100010000000000")
 except Exception as e:
-	print("[X] Timeout")
+	PAYLOAD = "73616d63000000004100010000000000".decode("hex")
+RESP_DETECT = b"samc"
+
+def scan_ip(ip_addr, timeout=TIMEOUT_SEC):
+	is_detected = False
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.settimeout(timeout)
+	try:
+		sock.connect((ip_addr, PORT))
+		sock.send(PAYLOAD)
+		recv_data = sock.recv(1024)
+		if recv_data.startswith(RESP_DETECT):
+			is_detected = True
+			print("[V] {}: CodeMeter instace detected!".format(ip_addr))
+		sock.close()
+	except Exception as e:
+		pass
+	if not is_detected:
+		print("[X] {}: Did not detect CodeMeter Runtime.".format(ip_addr))
+
+def main():
+	if len(sys.argv) < 2:
+		print("Usage: {} IP_ADDR_OR_SUBNET [TIMEOUT (optional)]".format(sys.argv[0]))
+		print("Example: {} 192.168.1.0/24 0.5".format(sys.argv[0]))
+		sys.exit(1)
+
+	try:
+		ip_addresses = IPNetwork(sys.argv[1])
+		timeout = float(sys.argv[2]) if len(sys.argv) == 3 else TIMEOUT_SEC
+	except Exception as e:
+		print("Usage: {} IP_ADDR_OR_SUBNET [TIMEOUT (optional)]".format(sys.argv[0]))
+		print("Example: {} 192.168.1.0/24 0.5".format(sys.argv[0]))
+		sys.exit(1)
+
+	print("[-] CodeMeter Runtime Port Scanner Started")
+	print("[-] Starting to scan {} ({} IPs) with timeout of {} seconds".format(str(ip_addresses), len(ip_addresses), timeout))
+	for ip_addr in ip_addresses:
+		scan_ip(str(ip_addr), timeout=timeout)
+	print("[-] Done")
+
+main()
